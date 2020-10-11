99f5229521a920ac23cdf9a9f72fb006c7d998a5
gennadigennadigennadi <gennadijanzen@icloud.com>
2020-10-11 21:46:02 +0200

    fix Inject class

--- src/Annotation/Inject.php
+++ src/Annotation/Inject.php
@@ -5,14 +5,16 @@ declare(strict_types=1);
 namespace Reinfi\DependencyInjection\Annotation;
 
 use Attribute;
+use InvalidArgumentException;
 use Psr\Container\ContainerInterface;
 
+#[Attribute(Attribute::TARGET_PROPERTY|Attribute::TARGET_METHOD)]
 
-//#[Attribute]
 /**
  * @package Reinfi\DependencyInjection\Annotation
  *
  * @Annotation
+ * @Target({"PROPERTY", "METHOD"})
  */
 final class Inject implements AnnotationInterface
 {
@@ -21,9 +23,25 @@ final class Inject implements AnnotationInterface
      */
     public $value;
 
-    public function __construct($value)
+    public function __construct($class = null)
     {
-        $this->value = $value;
+        if (null === $class) {
+            return;
+        }
+
+        if (\is_array($class)) {
+            $this->value = $class['value'];
+
+            return;
+        }
+
+        if (\is_string($class)) {
+            $this->value = $class;
+
+            return;
+        }
+
+        throw new InvalidArgumentException();
     }
 
     /**
